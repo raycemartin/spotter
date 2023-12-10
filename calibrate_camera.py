f3293205076dc9a2f818a753e3ebc064ca510c45
Rayce Martin <53666496+raycemartin@users.noreply.github.com>
2023-12-09 21:36:28 -0500

    Attempting stereo disparity

---
+++ calibrate_camera.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec  9 21:34:44 2023
+
+@author: rayce
+"""
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
--- stereo_video.py
+++ stereo_video.py
@@ -9,20 +9,29 @@ import cv2
 import numpy as np
 import sys
 
-s = 1
+s = 0
 if len(sys.argv) > 1:
     s = sys.argv[1]
     
 sensitivity = 15    
 source = cv2.VideoCapture(s)
 
-window_name = 'Green Object'
+window_name = 'Camera'
 cv2.namedWindow(window_name, cv2.WINDOW_NORMAL)
 
 while cv2.waitKey(1) != 27: #Escape
     has_frame, frame = source.read()
     frame = cv2.flip(frame, 0)
-    cv2.imshow('Original',frame)
-
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    
+    left_img = gray[:,:int(gray.shape[1]/2)]
+    right_img = gray[:,int(gray.shape[1]/2):]
+    
+    stereo = cv2.StereoBM_create(numDisparities=32, blockSize=15)
+    disparity = stereo.compute(left_img, right_img)
+    cv2.imshow('Disparity', disparity)
+    # cv2.imshow('Left',left_img)
+    # cv2.imshow('Right',right_img)
+    # cv2.imshow('Original',frame)
 source.release()
 cv2.destroyAllWindows() 
