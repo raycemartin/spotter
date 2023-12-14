377b9612176c62bd00a71dad0b8882f7c1c48b3d
Rayce Martin <53666496+raycemartin@users.noreply.github.com>
2023-12-13 21:43:00 -0500

    Added a class for the different cameras

--- calibrate_camera.py
+++ calibrate_camera.py
@@ -13,6 +13,56 @@ import time
 import os
 import glob
 
+class Camera:
+    def __init__(self, name):
+        self.name = name
+        self.camera_params = []
+        
+        
+    def calibrate(self):
+        ### Calibration ###   
+        
+        
+        size_of_target = (7*5, 3)
+        
+        # termination criteria
+        criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
+        
+        # prepare object points, like (0,0,0), (2,0,0) ..., (6,5,0)
+        objp = np.zeros(size_of_target, np.float32)
+        objp[:,:2] = np.mgrid[0:7,0:5].T.reshape(-1,2)
+        
+        # arrays to store object points and image points from all the images
+        objpoints = [] # 3d points in real space
+        imgpoints = [] # 2d points in real space
+        
+        images = glob.glob('*/*.png')
+        
+        for fname in images:
+            
+            img = cv2.imread(fname)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            
+            # find the chess board corners
+            ret, corners = cv2.findChessboardCorners(gray, (7,5), None)
+            
+            # if found, add object points, image poinrts (after refining them)
+            if ret == True:
+                print(fname)
+                objpoints.append(objp)
+                
+                corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
+                imgpoints.append(corners2)
+                
+                # draw and display corners
+                cv2.drawChessboardCorners(img, (7,5), corners2, ret)
+                cv2.imshow('img', img)
+                cv2.waitKey(500)
+        
+        cv2.destroyAllWindows()
+        self.camera_params = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+ 
+
 
 
 def take_images():
@@ -46,58 +96,16 @@ def take_images():
     source.release()
     cv2.destroyAllWindows() 
 
-def calibrate():
-    ### Calibration ###   
-    
-    size_of_target = (7*5, 3)
-    
-    # termination criteria
-    criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
-    
-    # prepare object points, like (0,0,0), (2,0,0) ..., (6,5,0)
-    objp = np.zeros(size_of_target, np.float32)
-    objp[:,:2] = np.mgrid[0:7,0:5].T.reshape(-1,2)
-    
-    # arrays to store object points and image points from all the images
-    objpoints = [] # 3d points in real space
-    imgpoints = [] # 2d points in real space
-    
-    images = glob.glob('*/*.png')
-    
-    for fname in images:
-        
-        img = cv2.imread(fname)
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        
-        # find the chess board corners
-        ret, corners = cv2.findChessboardCorners(gray, (7,5), None)
-        
-        # if found, add object points, image poinrts (after refining them)
-        if ret == True:
-            print(fname)
-            objpoints.append(objp)
-            
-            corners2 = cv2.cornerSubPix(gray, corners, (11,11), (-1,-1), criteria)
-            imgpoints.append(corners2)
-            
-            # draw and display corners
-            cv2.drawChessboardCorners(img, (7,5), corners2, ret)
-            cv2.imshow('img', img)
-            cv2.waitKey(500)
-    
-    cv2.destroyAllWindows()
-    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
-    return ret, mtx, dist, rvecs, tvecs 
 
 
-ret, mtx, dist, rvecs, tvecs = calibrate()
+# ret, mtx, dist, rvecs, tvecs = calibrate()
 
-img = cv2.imread(r'calibration_images/cal_left_img_12.png')
-h, w = img.shape[:2]
-newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
+# img = cv2.imread(r'calibration_images/cal_left_img_12.png')
+# h, w = img.shape[:2]
+# newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx, dist, (w,h), 1, (w,h))
 
-dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+# dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
 
-x,y,w,h = roi
-dst = dst[y:y+h, x:x+w]
-cv2.imwrite('calibration.png', dst)
+# x,y,w,h = roi
+# dst = dst[y:y+h, x:x+w]
+# cv2.imwrite('calibration.png', dst)
